d2f89d9dfcfe50783cf43a697eccbea7bb480cf0
MYT-Mayrose <mayrose.pasasadaba@module-zero.com>
2024-06-09 22:36:19 +0800

    begin design layout

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,19 +1,108 @@
 // Libraries
-import React from "react";
+import React, {useState, useRef} from "react";
+import axios from "axios";
 import { Container } from "react-bootstrap";
-import { Row, Col } from "antd";
+import { Row, Col, Layout, Menu, Divider } from "antd";
 // Components
 // Resourses and styles
-import logo from "../../assets/images/logo.png";
-import "../../styles/CommonStyles.css"
+import profile from "../../assets/images/logo.png";
+import "../../styles/CommonStyles.css";
+import { Content, Header } from "antd/es/layout/layout";
+import Title from "antd/es/typography/Title";
 
 export default function Homepage() {
+  const [loremIpsum, setLoremIpsum] = useState("");
+  const sections = [
+    { key: "home", label: "Home" },
+    { key: "about", label: "About" },
+    { key: "contact", label: "Contact" },
+    { key: "project", label: "Projects" },
+  ];
+  const projectRef = useRef();
+  const contactRef = useRef();
+
+  const scrollToSection = (sectionRef) => {
+    sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const handleMenuClick = (e) => {
+    switch (e.key) {
+      case 'project':
+        scrollToSection(projectRef);
+        break;
+      default:
+        break;
+    }
+  }
+
+  React.useEffect(() => {
+    const fetchLoremIpsum = async () => {
+      try {
+        const response = await axios.get('https://baconipsum.com/api/?type=all-meat&paras=2&format=text');
+        setLoremIpsum(response.data);
+      } catch (error) {
+        console.error('Error fetching Lorem Ipsum text', error);
+      }
+    };
+
+    fetchLoremIpsum();
+  }, []);
+
   return (
-    <Container fluid className="center">
-      <Row className="d-flex justify-content-center">
-          <img src={logo} alt="logo" className="w-25"/>
-      </Row>
-      <Row>WELCOME TO THE HOMEPAGE</Row>
-    </Container>
+    <Layout className="main-layout">
+      <Header style={{ display: "flex", alignItems: "center" }}>
+        <div className="demo-logo" />
+        <Title level={5} className="header-title">Maywose Portfolio</Title>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          items={sections}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+          className="menu-container"
+          onClick={handleMenuClick}
+        />
+      </Header>
+      <Content className="main-content">
+        <Container fluid className="">
+          <Row className="profile-container">
+            <Col xs={24} md={24} lg={12} className="center left-section">
+              <div>
+                <Row>Hello, I'm Mayrose,</Row>
+                <Row><Title level={1} className="specialty-title">Frontend Developer</Title></Row>
+                <Row><u onClick={()=>scrollToSection(contactRef)} className="pointer">Contact Me</u></Row>
+              </div>
+            </Col>
+            <Col xs={24} md={24} lg={12} className="center right-section">
+              <div className="image-container">
+                <img src={profile} alt="img" className="w-100"/>
+              </div>
+            </Col>
+          </Row>
+
+            <Row className="d-flex justify-content-center">
+              <Title level={3} className="specialty-title">About</Title>
+            </Row>
+            <Row className="section-paragraph"><p>{loremIpsum}</p></Row>
+
+            <Row className="d-flex justify-content-center">
+              <Title level={3} className="specialty-title">Skills</Title>
+            </Row>
+            <Row className="section-paragraph"><p>{loremIpsum}</p></Row>
+
+            <Row className="d-flex justify-content-center" ref={projectRef}>
+              <Title level={3} className="specialty-title">Projects</Title>
+            </Row>
+            <Row className="section-paragraph"><p>{loremIpsum}</p></Row>
+
+            <Row ref={contactRef} className="d-flex justify-content-center">
+              <Title level={3} className="specialty-title">Contact Me</Title>
+            </Row>
+            <Row className="section-paragraph"><p>{loremIpsum}</p></Row>
+        </Container>
+      </Content>
+    </Layout>
   );
 }
